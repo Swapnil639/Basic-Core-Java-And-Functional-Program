b5f4bc3eed5f95505a9feddd449839902d4d0854
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 09:16:22 +0530

    Added :Find year is leap year or not

--- src/com/bridgelabz/LeapYear.java
+++ src/com/bridgelabz/LeapYear.java
@@ -1,2 +1,16 @@
-package com.bridgelabz;public class LeapYear {
+package com.bridgelabz;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        int year=Utility.scannerInt();
+        if (isLeapYear(year)){
+            System.out.println(year+" is Leap year");
+        }else {
+            System.out.println(year+" is not Leap year");
+        }
+    }
+
+    private static boolean isLeapYear(int year) {
+       return year%400==0 || year%100!=0 && year%4==0;
+    }
 }
