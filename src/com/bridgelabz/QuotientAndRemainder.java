8b106f599964521bdc552f64573438a70eedcd0c
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 09:19:19 +0530

    Added :Compute Quotient and Remainder

--- src/com/bridgelabz/QuotientAndRemainder.java
+++ src/com/bridgelabz/QuotientAndRemainder.java
@@ -1,2 +1,17 @@
-package com.bridgelabz;public class QuotientAndRemainder {
+package com.bridgelabz;
+
+public class QuotientAndRemainder {
+    public static void main(String[] args) {
+        int dividend=20;
+        int divisor=4;
+        computeQuotientAndRemainder(dividend,divisor);
+    }
+
+    public static void computeQuotientAndRemainder(int dividend, int divisor) {
+        int quotient = dividend / divisor;
+        int remainder = dividend % divisor;
+
+        System.out.println("Quotient: " + quotient);
+        System.out.println("Remainder: " + remainder);
+    }
 }
