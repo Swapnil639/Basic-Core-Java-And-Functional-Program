06eb7c7f50e00d6e1bbc27f0e89a4f82d02bbc9c
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 09:17:48 +0530

    Added :Print power of 2 number

--- src/com/bridgelabz/PowerOf2.java
+++ src/com/bridgelabz/PowerOf2.java
@@ -1,2 +1,16 @@
-package com.bridgelabz;public class PowerOf2 {
+package com.bridgelabz;
+
+public class PowerOf2 {
+    public static void main(String[] args) {
+        int n=Utility.scannerInt();
+        printPowerOfTwo(n);
+    }
+
+    public static void printPowerOfTwo(int n) {
+        int power=1;
+        for (int i = 0; i <=n; i++) {
+            System.out.println("2^"+i+"="+power);
+            power=power*2;
+        }
+    }
 }
