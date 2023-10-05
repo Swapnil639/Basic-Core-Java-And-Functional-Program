9d8d9ab4c9e51c0a28624991482a4fb2245329fc
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 15:56:15 +0530

    Added :Swap two number

--- src/com/bridgelabz/SwapNumber.java
+++ src/com/bridgelabz/SwapNumber.java
@@ -1,2 +1,18 @@
-package com.bridgelabz;public class SwapNumber {
+package com.bridgelabz;
+
+
+public class SwapNumber {
+    public static void main(String[] args) {
+        int a=Utility.scannerInt();
+        int b=Utility.scannerInt();
+        swapNumber(a,b);
+    }
+
+    public static void swapNumber(int a, int b) {
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a:"+a);
+        System.out.println("b:"+b);
+    }
 }
