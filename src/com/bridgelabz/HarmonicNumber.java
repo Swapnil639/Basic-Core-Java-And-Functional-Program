771fca3176bab8f2169071f9bc2adae1b805a3ae
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 09:11:52 +0530

    Added :Print Nth Harmonic value

--- src/com/bridgelabz/HarmonicNumber.java
+++ src/com/bridgelabz/HarmonicNumber.java
@@ -1,2 +1,18 @@
-package com.bridgelabz;public class HarmonicNumber {
+package com.bridgelabz;
+
+public class HarmonicNumber {
+    public static void main(String[] args) {
+        int num=Utility.scannerInt();
+        System.out.println(computeNthHarmonicNumber(num));
+    }
+
+    public static double computeNthHarmonicNumber(int num) {
+        double harmonicNumber = 0.0;
+
+        for (int i = 1; i <= num; i++) {
+            harmonicNumber += 1.0 / i;
+        }
+
+        return harmonicNumber;
+    }
 }
