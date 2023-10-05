4ec2899db4317f0512fc6d6c309e6618be90fbf0
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 15:55:02 +0530

    Added :Print distinct triplets

--- src/com/bridgelabz/SumOfThreeInteger.java
+++ src/com/bridgelabz/SumOfThreeInteger.java
@@ -1,2 +1,20 @@
-package com.bridgelabz;public class SumOfThreeInteger {
+package com.bridgelabz;
+
+public class SumOfThreeInteger {
+    public static void main(String[] args) {
+        int[] arr = {1, -2, 3, 0, -1, 2, -3, 4};
+        sumOfThreeInteger(arr);
+    }
+
+    public static void sumOfThreeInteger(int[] arr) {
+        for (int i = 0; i < arr.length - 2; i++) {
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                for (int k = j + 1; k < arr.length; k++) {
+                    if (arr[i] + arr[j] + arr[k] == 0) {
+                        System.out.println("Distinct triplets :" + arr[i] + "," + arr[j] + "," + arr[k]);
+                    }
+                }
+            }
+        }
+    }
 }
