092376f689109f84c087a4b1c82808a9e726afd1
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 09:14:58 +0530

    Added :Print Largest among three number

--- src/com/bridgelabz/LargeAmongThreeNumber.java
+++ src/com/bridgelabz/LargeAmongThreeNumber.java
@@ -1,2 +1,21 @@
-package com.bridgelabz;public class LargeAmongThreeNumber {
+package com.bridgelabz;
+
+public class LargeAmongThreeNumber {
+    public static void main(String[] args) {
+        int a=Utility.scannerInt();
+        int b=Utility.scannerInt();
+        int c=Utility.scannerInt();
+        findLargeNumber(a,b,c);
+
+    }
+
+    public static void findLargeNumber(int a,int b,int c) {
+        if (a>b && a>c){
+            System.out.println("a is largest number");
+        } else if (b>a && b>c) {
+            System.out.println("b is largest number");
+        }else {
+            System.out.println("c is largest number");
+        }
+    }
 }
