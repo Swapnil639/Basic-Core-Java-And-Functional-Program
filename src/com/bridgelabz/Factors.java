b8f9cff04b73b1c0a223e48b9f7df6171cb64c1b
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 09:10:39 +0530

    Added :Print prime factor of number N

--- src/com/bridgelabz/Factors.java
+++ src/com/bridgelabz/Factors.java
@@ -1,2 +1,17 @@
-package com.bridgelabz;public class Factors {
+package com.bridgelabz;
+
+public class Factors {
+    public static void main(String[] args) {
+        int i=2,temp;
+        int n=Utility.scannerInt();
+        temp=n;
+        while (temp>1){
+            if (temp%i==0){
+                System.out.println(i+",");
+                temp=temp/i;
+            }else {
+                i++;
+            }
+        }
+    }
 }
