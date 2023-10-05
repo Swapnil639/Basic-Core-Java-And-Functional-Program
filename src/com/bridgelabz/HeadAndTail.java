96c984deec80f661d668a01ab055add1e50ef9c7
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 09:13:09 +0530

    Added :Print Percentage of Head vs Tails

--- src/com/bridgelabz/HeadAndTail.java
+++ src/com/bridgelabz/HeadAndTail.java
@@ -1,2 +1,31 @@
-package com.bridgelabz;public class HeadAndTail {
+package com.bridgelabz;
+
+import java.util.Random;
+
+public class HeadAndTail {
+    public static void main(String[] args) {
+        int numFlips=100;
+        computePercentageOfHeadsAndTails(numFlips);
+    }
+
+    public static void computePercentageOfHeadsAndTails(int numFlips) {
+        Random random = new Random();
+        int headsCount = 0;
+        int tailsCount = 0;
+
+        for (int i = 0; i < numFlips; i++) {
+            double randomValue = random.nextDouble();
+            if (randomValue < 0.5) {
+                tailsCount++;
+            } else {
+                headsCount++;
+            }
+        }
+
+        double headsPercentage = (double) headsCount / numFlips * 100;
+        double tailsPercentage = (double) tailsCount / numFlips * 100;
+
+        System.out.println("Percentage of Heads: " + headsPercentage);
+        System.out.println("Percentage of Tails: " + tailsPercentage);
+    }
 }
