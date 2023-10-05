3068738e3c5b6d0a03f7632181cefb84fec5a705
Swapnil Patil <swapnilpatil631997@gmail.com>
2023-10-05 15:58:01 +0530

    Added :Check Whether an Alphabet is Vowel or Consonant

--- src/com/bridgelabz/VowelOrConsonant.java
+++ src/com/bridgelabz/VowelOrConsonant.java
@@ -1,2 +1,33 @@
-package com.bridgelabz;public class VowelOrConsonent {
+package com.bridgelabz;
+
+public class VowelOrConsonant {
+    public static void main(String[] args) {
+        System.out.print("Enter one character:");
+        char ch=Utility.scannerChar();
+        findVowelOrConsonant(ch);
+    }
+
+    public static void findVowelOrConsonant(char ch) {
+        switch (ch){
+            case 'a':
+                System.out.println(ch+" is Vowel");
+                break;
+                case 'e':
+                System.out.println(ch+" is Vowel");
+                break;
+                case 'i':
+                System.out.println(ch+" is Vowel");
+                break;
+                case 'o':
+                System.out.println(ch+" is Vowel");
+                break;
+                case 'u':
+                System.out.println(ch+" is Vowel");
+                break;
+            default:
+                System.out.println(ch+" is Consonant");
+                break;
+        }
+    }
+
 }
